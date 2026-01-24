ec64491202ad47c6237dbd84fe22d65e35e30a49
지우 <jiwoo2534@gmail.com>
2026-01-24 15:21:48 +0900

    Update database connection credentials and session handling

--- db_conn.php
+++ db_conn.php
@@ -1,7 +1,12 @@
 <?php
-$conn = mysqli_connect("localhost", "root", "341205", "library_db");
+$conn = mysqli_connect("localhost", "root", "password", "library_db");
+
 if (!$conn) {
     die("연결 실패: " . mysqli_connect_error());
 }
 mysqli_set_charset($conn, "utf8mb4");
+
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
 ?>
