ce852f557eb1bcb504e53aa8547ff39af5387c08
지우 <jiwoo2534@gmail.com>
2026-01-24 14:55:47 +0900

    Refactor index.php for improved readability

--- index.php
+++ index.php
@@ -1,8 +1,7 @@
 <?php
-// DB 연결 및 세션 시작 파일 포함
 include "db_conn.php";
 
-// 로그인 세션(user_id)이 없으면 로그인 페이지로 강제 이동
+// 로그인 세션(user_id)이 없으면 로그인 페이지로 이동
 if (!isset($_SESSION['user_id'])) {
     header("Location: login.php");
     exit;
@@ -47,7 +46,7 @@ if (!isset($_SESSION['user_id'])) {
 </head>
 <body>
     <div class="success-box">
-        <h1>로그인 성공!</h1>
+        <h1>로그인 성공</h1>
         <p><span class="user-name"><?php echo $_SESSION['user_name']; ?></span> 님, 환영합니다.</p>
         <p>로그인 처리가 정상적으로 완료되어 <strong>index.php</strong>로 이동했습니다.</p>
         <a href="logout.php" class="logout-btn">로그아웃</a>
@@ -55,3 +54,4 @@ if (!isset($_SESSION['user_id'])) {
 </body>
 
 </html>
+
